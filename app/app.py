edfff1a090bc744dd40d6e33e006a243712b5acc
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2020-07-02 13:06:40 +0800

    initial commit

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+from mock_output import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'wello horld'
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    app.logger.info('*' * 30)
+    app.logger.info('request made to /search')
+    app.logger.info(request.json)
+    app.logger.info('*' * 30)
+
+    if request.json['target'] == 'variableQuery2':
+        return jsonify(search_endpoint_array2)
+
+    if request.json['target'] == 'variableQuery3':
+        return jsonify(search_endpoint_map)
+
+    # the default
+    return jsonify(search_endpoint_array)
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    app.logger.info('*' * 30)
+    app.logger.info('request made to /query')
+    app.logger.info(request.json)
+    app.logger.info('*' * 30)
+    return jsonify(search_endpoint_array)
+
+@app.route('/annotations', methods=['GET', 'POST'])
+def annotations():
+    app.logger.info('*' * 30)
+    app.logger.info('request made to /annotations')
+    app.logger.info(request.json)
+    app.logger.info('*' * 30)
+    return jsonify(search_endpoint_array)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ app/mock_output.py
@@ -0,0 +1,23 @@
+search_endpoint_array = [
+    'foo',
+    'bar',
+    'baz'
+]
+
+search_endpoint_array2 = [
+    'foo2',
+    'bar2',
+    'baz2'
+]
+
+search_endpoint_map = [
+    {
+        "text": "upper_25",
+        "value": 1
+    },
+    {
+        "text": "upper_75",
+        "value": 2
+    }
+]
+
